553359ced9253081c83a19ca4193214dcf724901
kausar <kausar.alm@gmail.com>
2016-03-21 14:00:31 +0600

    Bare underscore theme committed

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package themeforexm
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-for-exm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'theme-for-exm' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-for-exm' ), 'theme-for-exm', '<a href="http://www.kausaralam.com" rel="designer">Kausar Alam</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
